9f6e05e59367f6b023d48c76f16f5760072c8266
Sabin Adams <sabintheworld@gmail.com>
2022-02-01 03:24:06 -0800

    Finished sample

--- app/routes/profile.$username.tsx
+++ app/routes/profile.$username.tsx
@@ -1,4 +1,9 @@
-import { MetaFunction, json, LoaderFunction, useLoaderData, redirect } from 'remix'
+import { 
+    json,
+    LoaderFunction,
+    useLoaderData,
+    MetaFunction
+} from 'remix'
 
 type User = {
     username: string;
@@ -9,9 +14,15 @@ type User = {
 export const meta: MetaFunction = ({ data }: { data: User }) => {
     const formatName = (name: string) => name.charAt(0).toUpperCase() + name.slice(1)
     return {
-        title: `${formatName(data.username)}'s Profile Page`
+      title: `${formatName(data.username)}'s Profile Page`,
+      'twitter:card': 'summary_large_image',
+      'twitter:creator': `@${data.username}`,
+      'twitter:site': `@${data.username}`,
+      'twitter:title': `${data.first} ${data.last}`,
+      'twitter:description': `${data.first} ${data.last}'s profile page. Check it out @${data.username}`
     }
 }
+
 
 export let loader: LoaderFunction = async ({ params }) => {
     const users: User[] = [
@@ -41,8 +52,8 @@ export default function Profile() {
     const user = useLoaderData()
 
     return (
-        <div style={{ display: 'flex', height: '100%', justifyContent: 'center', alignItems: 'center'}}>
-            <h2>{ user.first }</h2>
+        <div style={{ display: 'flex', height: '100%', justifyContent: 'center', alignItems: 'center', background: '#0c0f12' }}>
+            <h2 style={{ flex: 1, textAlign: 'center', color: '#f1f1f1', fontFamily: 'system-ui' }}>{user.first} {user.last}</h2>
         </div>
     );
 }
